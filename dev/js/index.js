a44e1ac21523a6066d630d59f6c175ce50f4ea9b
Sulabh <sulabh4@hotmail.com>
2018-02-26 00:24:45 +0545

    Completed Front End for React CRUD Application

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,18 @@
 import 'babel-polyfill';
+
+// React
 import React from 'react';
 import ReactDOM from "react-dom";
+
+// Material-UI
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// Components
 import App from './App.js';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render( 
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <App />
+    </MuiThemeProvider>
+    , document.getElementById('root'));
